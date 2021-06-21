e83ce3d55580a4ab705cf266ce4a5eb773750fd7
Shaoxiong <shaoxiongdu.dev@gmail.com>
2021-06-21 21:59:05 +0800

    新增 虚拟机栈

---
+++ demo/src/cn/shaoxiongdu/chapter2/codeFlowerTest.java
@@ -0,0 +1,13 @@
+package cn.shaoxiongdu.chapter2;
+
+public class codeFlowerTest {
+
+    public static void main(String[] args) {
+
+        byte i= 15;
+        int j = 8;
+        int k = i + j;
+
+    }
+
+}
