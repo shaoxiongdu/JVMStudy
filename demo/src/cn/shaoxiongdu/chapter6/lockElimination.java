1c9fa718e7a105477c4941b467502df025aa12e6
shaoxiongdu <email@shaoxiongdu.cn>
2021-08-03 14:26:01 +0800

    完成-高校并发-锁优化

---
+++ demo/src/cn/shaoxiongdu/chapter6/lockElimination.java
@@ -0,0 +1,14 @@
+package cn.shaoxiongdu.chapter6;
+
+/**
+ * @作者: 写Bug的小杜 【email@shaoxiongdu.cn】
+ * @时间: 2021/08/02
+ * @描述:
+ */
+public class lockElimination {
+
+    public String concat(String s1, String s2){
+        return s1 + s2;
+    }
+
+}
